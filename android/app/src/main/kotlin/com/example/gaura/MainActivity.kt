dae50ad66b32cf052986f2a41488bf94806a09d6
gauransh18 <66783709+gauransh18@users.noreply.github.com>
2023-04-21 12:56:33 +0530

    frontend90

---
+++ android/app/src/main/kotlin/com/example/gaura/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gaura
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_constructors
+
+
+import 'package:gaura/home.dart';
+
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'hello',
+      home:
+
+          Home(),
+
+
+    );
+  }
+}
